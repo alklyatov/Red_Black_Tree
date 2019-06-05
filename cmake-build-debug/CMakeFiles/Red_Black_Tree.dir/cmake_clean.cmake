1c61133db1d4fb7156de583a7205561cea4fbf03
alklyatov <alklyatov@mail.ru>
2019-06-06 00:53:53 +0300

    Red_Black_Tree

---
+++ cmake-build-debug/CMakeFiles/Red_Black_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Red_Black_Tree.dir/main.cpp.obj"
+  "Red_Black_Tree.pdb"
+  "Red_Black_Tree.exe"
+  "Red_Black_Tree.exe.manifest"
+  "libRed_Black_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Red_Black_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
